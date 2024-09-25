7e1f9bc989aa9e6567d1db7c5c0224a41c208b62
minseonnnn <mm1687d@naver.com>
2024-09-25 11:37:08 +0900

    0925

---
+++ SpringFrontProject_6/src/main/java/com/sist/dao/FoodDAO.java
@@ -0,0 +1,56 @@
+package com.sist.dao;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.FoodMapper;
+import com.sist.vo.*;
+@Repository
+public class FoodDAO {
+	@Autowired
+    private FoodMapper mapper;
+    
+    /*
+     *   @Select("SELECT fno,poster,type,name,num "
+				  +"FROM (SELECT fno,poster,type,name,rownum as num "
+				  +"FROM (SELECT fno,poster,type,name "
+				  +"FROM project_food_house "
+				  +"WHERE type LIKE '%'||#{type}||'%' ORDER BY fno)) "
+				  +"WHERE num BETWEEN #{start} AND #{end}")
+		  public List<FoodVO> foodTypeListData(Map map);
+		  
+		  @Select("SELECT CEIL(COUNT(*)/20.0) FROM project_food_house "
+				  +"WHERE type LIKE '%'||#{type}||'%'")
+		  public int foodTypeTotalPage(String type);
+     */
+    public List<FoodVO> foodTypeListData(Map map)
+    {
+    	return mapper.foodTypeListData(map);
+    }
+    public int foodTypeTotalPage(String type)
+    {
+    	return mapper.foodTypeTotalPage(type);
+    }
+    // 상세보기
+    /*
+     *    // 상세보기
+		  @Update("UPDATE project_food_house SET "
+				  +"hit=hit+1 "
+				  +"WHERE fno=#{fno}")
+		  public void hitIncrement(int fno);
+		  @Select("SELECT fno,name,type,phone,address,theme,poster,images,time,"
+				  +"content,score "
+				  +"FROM project_food_house "
+				  +"WHERE fno=#{fno}")
+		  public FoodVO foodDetailData(int fno);
+     */
+    public FoodVO foodDetailData(int fno)
+    {
+    	mapper.hitIncrement(fno);
+    	return mapper.foodDetailData(fno);
+    }
+}
