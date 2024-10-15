7d7199c0f3f7a87c6ab7bd356539a2dba398a145
minseonnnn <mm1687d@naver.com>
2024-10-15 16:02:07 +0900

    1015

--- SpringLastProject/src/main/java/com/sist/web/MyPageController.java
+++ SpringLastProject/src/main/java/com/sist/web/MyPageController.java
@@ -8,11 +8,23 @@ public class MyPageController {
   @GetMapping("mypage/mypage_main.do")
   public String mypage_main()
   {
+	  
 	  return "mypage";
   }
   @GetMapping("mypage/mypage_reserve.do")
   public String mypage_reserve()
   {
 	  return "mypage/mypage_reserve";
+  }
+  
+  @GetMapping("mypage/mypage_cart.do")
+  public String mypage_cart()
+  {
+	  return "mypage/mypage_cart";
+  }
+  @GetMapping("mypage/mypage_buy.do")
+  public String mypage_buy()
+  {
+	  return "mypage/mypage_buy";
   }
 }
